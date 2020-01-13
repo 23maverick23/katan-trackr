40ef19938aa78140899cf58af3e2ef8e18a5b1b6
Ryan Morrissey <me@rymo.io>
2020-01-12 22:49:51 -0700

    Add a manual override to debug

--- katan_trackr/settings/prod.py
+++ katan_trackr/settings/prod.py
@@ -1,6 +1,7 @@
+import os
 from .base import *
 import dj_database_url
 
-DEBUG = False
+DEBUG = bool(os.environ.get('DEBUG_OVERRIDE', False))
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
