ec484823f491612a469ace1d9c618791e9309fa5
Ryan Morrissey <me@rymo.io>
2020-01-12 22:58:17 -0700

    Add logging config to settings

--- katan_trackr/settings/base.py
+++ katan_trackr/settings/base.py
@@ -142,5 +142,40 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
+# Logging config
+DEBUG_PROPAGATE_EXCEPTIONS = True
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                       'pathname=%(pathname)s lineno=%(lineno)s ' +
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'infologger': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        }
+    }
+}
+
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
